410e5333c0fbb8770b93c47de97fdba040df4b30
suzyrhouser@gmail.com <suzyrhouser@gmail.com@7ee15593-3f35-b33e-a972-c07f9e855287>
2014-05-06 15:53:05 +0000

    Questionable functions for wrist/claw; defined analog channels

--- Tomcat_Surface.X/Tomcat_S_main.c
+++ Tomcat_Surface.X/Tomcat_S_main.c
@@ -1,5 +1,14 @@
 #include "Tomcat_S_driver.h"
 
+#define PORT 1
+#define STBD 2
+#define VERT 3
+#define LAT 4
+//#define CLAW 5
+//#define WRIST 6
+#define TILT 5
+#define PAN 6
+
 //variables
 char n = 0;
 char recv_buff = 0;
@@ -8,6 +17,8 @@ char recv_flag = 0;
 //functions
 void tx_chars(char tx_buff[],char num_byte);
 unsigned int read_an(char); //love, suzyrhouser
+unsigned int get_claw(void);
+unsigned int get_claw(void);
 
 //interupts
 
@@ -41,14 +52,14 @@ void main(void) {
     while (1) {
         //                if (recv_flag) {
         buff[0] = 0xff;
-        buff[1] = PORTBbits.RB0;
-        buff[2] = PORTBbits.RB1;
-        buff[3] = PORTBbits.RB2;
-        buff[4] = PORTBbits.RB3;
-        buff[5] = PORTBbits.RB4;
-        buff[6] = PORTBbits.RB5;
-        buff[7] = PORTDbits.RD6;
-        buff[8] = PORTDbits.RD7;
+        buff[1] = read_an(PORT); //Port Thruster Speed
+        buff[2] = read_an(STBD); //Stbd Thruster Speed
+        buff[3] = read_an(VERT); //Vert Thruster Speed
+        buff[4] = read_an(LAT); //Lat Thruster Speed
+ //       buff[5] = read_an(CLAW); //Claw
+ //       buff[6] = read_an(WRIST); //Wrist
+        buff[5] = read_an(TILT); //Tilt
+        buff[6] = read_an(PAN); //Pan
 
 
         tx_chars(buff,9);
@@ -76,4 +87,20 @@ unsigned int read_an(char channel)
     while(ADCON0bits.GO); //wait til done processing
     num = ADRES; //value that you want
     return(num); //here ya go
+}
+
+unsigned int get_claw(void)
+{
+    if ("claw_open_pin" = 1)
+       claw_open = 187;
+    if ("claw_close_pin" =1)
+        claw_close = 67;
+}
+
+unsigned int get_wrist(void)
+{
+    if ("wrist_open_pin" = 1)
+       wrist_open = 187;
+    if ("wrist_close_pin" =1)
+        wrist_close = 67;
 }
